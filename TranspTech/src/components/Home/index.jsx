b16cccc695019d8258d0eef5ac39125420b945bb
Lucas Gabriel <lucasgabrielrs123@gmail.com>
2024-04-01 11:20:23 -0300

    variaveis e home

---
+++ TranspTech/src/components/Home/index.jsx
@@ -0,0 +1,19 @@
+
+import { useState,useEffect } from 'react'
+import './style/Home.css'
+
+const Home = () => {
+    
+    return (
+        <>
+            <section className='hero'>
+                <div className="container">
+                    <h2>Soluções Web para <br /><strong>Gigantes da Estrada</strong></h2>
+                    <p>A sua frota percorre as estradas com eficiência;<br></br> o seu site deve navegar pela web com a mesma destreza. <br></br>Oferecemos soluções web que expandem seu alcance e aceleram seus resultados.</p>
+                </div>    
+             </section>
+        </>
+    )
+}
+
+export default Home;
--- TranspTech/src/pages/Homepage.jsx
+++ TranspTech/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from '../components/Header/index'
+import Home from '../components/Home'
 
 const HomePage = () => {
     return (
       <>
         {/*Aqui são chamados os componentes da Homepage*/}
         <Header />
+        <Home />
       </>
     )
   }
