468e3fca87029c21edc3d686ea511cf46951b32c
Lucas Gabriel <lucasgabrielrs123@gmail.com>
2024-04-02 11:26:55 -0300

    botoes

--- TranspTech/src/components/Home/index.jsx
+++ TranspTech/src/components/Home/index.jsx
@@ -10,8 +10,11 @@ const Home = () => {
                 <div className="container">
                     <h2>Soluções Web para <br /><strong>Gigantes da Estrada</strong></h2>
                     <p>A sua frota percorre as estradas com eficiência;<br></br> o seu site deve navegar pela web com a mesma destreza. <br></br>Oferecemos soluções web que expandem seu alcance e aceleram seus resultados.</p>
-                    <button>Conheça quem somos</button>
-                    <button>Veja os preços</button>
+                    <div className='container-buttons'>
+                        <button>Conheça quem somos</button>
+                        <button className='transparente'>Veja os preços</button>
+                    </div>
+                    
                 </div>    
              </section>
         </>
