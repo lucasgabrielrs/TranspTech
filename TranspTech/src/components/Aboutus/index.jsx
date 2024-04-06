0405593d44a1574b8735c4da95ae1be4be8ee380
Danilim04 <daniel.ferraz04@hotmail.com>
2024-04-06 16:04:28 -0300

    AjustesResponsividade

--- TranspTech/src/components/Aboutus/index.jsx
+++ TranspTech/src/components/Aboutus/index.jsx
@@ -5,11 +5,13 @@ const Aboutus = () => {
         <>
             <section className='aboutus'>
                 <div className='container-aboutus'>
+                    
                     <span>Sobre nós</span>
                     <h2>TranspTech</h2>
                     <p>Somos especialistas em desenvolver soluções web personalizadas para o setor de transportes. Com mais de duas décadas de experiência, combinamos tradição e tecnologia para potencializar sua logística e gestão.<br></br> Nossas plataformas são robustas, seguras e desenhadas para facilitar o seu trabalho no dia a dia.</p>
                 </div>
-                <img src="./image/aboutus.png" alt="imagem da equipe transptech" />
+                <img src='/public/image/aboutus.jpg' className="imagemAboutUs"></img>
+
             </section>
         </>
     )
--- TranspTech/src/components/Testimonial/index.jsx
+++ TranspTech/src/components/Testimonial/index.jsx
@@ -15,7 +15,7 @@ const Testimonial = () => {
                    </div>
                 </div>    
              </section>
-        </>
+        </> 
     )
 }
 
